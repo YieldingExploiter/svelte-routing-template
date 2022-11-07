5e263f3fff47a14047c0580aea543f71529923d9
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-07 06:36:28 +0100

    fix: :bug: wrong config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import sveltePreprocess from 'svelte-preprocess'
-import fs from 'fs'
 
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
-  build: {
-    base: fs.existsSync('.root-path') ? fs.readFileSync('.root-path', 'utf-8').trim() : '/'
-  }
 }
-
-console.log('Base Path:',config.base);
 
 export default config
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as fs from 'fs'
+
+const base = fs.existsSync('.root-path') ? fs.readFileSync('.root-path', 'utf-8').trim() : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base
 })
+
+
+console.log('Base Path:',base);
