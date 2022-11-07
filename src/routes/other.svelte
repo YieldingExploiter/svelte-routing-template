30557d0a5c4f6965dc4b9643853abae768b2fef8
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-07 07:11:54 +0100

    feat: :sparkles: add counter

--- src/routes/other.svelte
+++ src/routes/other.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Counter from '../lib/Counter.svelte';
   import Link from '../lib/Link.svelte';
 </script>
 
@@ -8,4 +9,7 @@
   This is the file at /src/routes/other.svelte (route /other) - <Link to="/"
     >Go back to index!</Link
   >
+  <br />
+  P.S: Here's a counter because why not
 </p>
+<Counter />
