b04bc14dfda7cfd9f638279239dbe08e4c808fa0
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-07 06:32:22 +0100

    chore: :loud_sound: add base path log

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import sveltePreprocess from 'svelte-preprocess'
 import fs from 'fs'
 
-export default {
+const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
   base: fs.existsSync('.root-path') ? `/${fs.readFileSync('.root-path','utf-8').trim()}` : '/'
 }
+
+console.log('Base Path:',config.base);
+
+export default config
